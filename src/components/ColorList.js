f51353f8f552f363654b44e11640c69e24cdfcb0
Kelvin Kapaya <kalvinkapu@gmail.com>
2023-07-20 12:13:33 +0300

    Completed Assignment

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,5 +1,7 @@
 import React from "react";
-
+function ColorItem(props){
+  return <li style = {{color:props.color}}>{props.color}</li>
+}
 function ColorList() {
   const colors = [
     "firebrick",
@@ -8,16 +10,21 @@ function ColorList() {
     "darkslategray",
     "hotpink",
   ];
-
+const colorElement= colors.map((color)=> {
+  return (<ColorItem key={color} color={color}/>
+);
+});
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
       <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
+        {/* <li style={{ color: colors[0] }}>{colors[0]}</li>
         <li style={{ color: colors[1] }}>{colors[1]}</li>
         <li style={{ color: colors[2] }}>{colors[2]}</li>
         <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
+        <li style={{ color: colors[4] }}>{colors[4]}</li> */}
+
+       {colorElement}
       </ol>
     </div>
   );
